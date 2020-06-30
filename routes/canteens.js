47725ae923ddefefc1979f3256384f872d12f27d
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-06-30 23:49:19 +0200

    Use Map for "/canteens" route data access

--- routes/canteens.js
+++ routes/canteens.js
@@ -7,33 +7,24 @@ const canteens = require('ka-mensa-fetch/data/canteens.json')
 // CONSTANTS
 
 /**
- * An objectwith the mapping: canteen id -> canteen.
+ * A map: canteen id -> canteen object.
  *
- * @type {object}
+ * @type {Map}
  */
 const CANTEENS_BY_ID = (() => {
-  const obj = {}
-  for (const item of canteens) {
-    obj[item.id] = item
-  }
-  return Object.freeze(obj)
+  return new Map(canteens.map(item => [item.id, item]))
 })()
 
 /**
- * An object with the mapping: canteen id -> (object: line id -> line).
+ * A map: canteen id -> (map: line id -> line object).
  *
- * @type {object}
+ * @type {Map}
  */
 const CANTEEN_LINES_BY_ID = (() => {
-  const obj = {}
-  for (const item of canteens) {
-    const lineObj = {}
-    for (const line of item.lines) {
-      lineObj[line.id] = line
-    }
-    obj[item.id] = lineObj
-  }
-  return Object.freeze(obj)
+  return new Map(canteens.map(canteen => [
+    canteen.id,
+    new Map(canteen.lines.map(line => [line.id, line]))
+  ]))
 })()
 
 // ROUTES
@@ -45,28 +36,28 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/:canteenId', async (req, res, next) => {
-  const canteen = CANTEENS_BY_ID[req.params.canteenId]
+  const canteen = CANTEENS_BY_ID.get(req.params.canteenId)
   if (!canteen) {
-    res.status(404).end()
+    res.status(404).json({})
     return
   }
   res.status(200).json(canteen)
 })
 
 router.get('/:canteenId/lines', async (req, res, next) => {
-  const canteen = CANTEENS_BY_ID[req.params.canteenId]
+  const canteen = CANTEENS_BY_ID.get(req.params.canteenId)
   if (!canteen) {
-    res.status(404).end()
+    res.status(404).json({})
     return
   }
   res.status(200).json(canteen.lines)
 })
 
 router.get('/:canteenId/lines/:lineId', async (req, res, next) => {
-  const linesObj = CANTEEN_LINES_BY_ID[req.params.canteenId]
-  const line = linesObj ? linesObj[req.params.lineId] : null
+  const linesMap = CANTEEN_LINES_BY_ID.get(req.params.canteenId)
+  const line = linesMap ? linesMap.get(req.params.lineId) : null
   if (!line) {
-    res.status(404).end()
+    res.status(404).json({})
     return
   }
   res.status(200).json(line)
