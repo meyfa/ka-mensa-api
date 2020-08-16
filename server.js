878517d2016de3553d7d98ccf5800e76c4287364
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2020-08-16 22:46:59 +0200

    Add logging with Winston

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 'use strict'
 
+const winston = require('winston')
 const express = require('express')
 const ms = require('ms')
 
 const config = require('./config')
-const runFetchJob = require('./lib/job');
+const runFetchJob = require('./lib/job')
+
+// LOG
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`
+    })
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
 
 // STARTUP ROUTINE
 
 (async () => {
   // setup fetch job
   const fetchInterval = ms(config.fetchJob.interval)
-  await runFetchJob()
-  setInterval(runFetchJob, fetchInterval)
+  await runFetchJob(logger)
+  setInterval(() => runFetchJob(logger), fetchInterval)
 
   const app = express()
 
@@ -28,6 +44,6 @@ const runFetchJob = require('./lib/job');
   const port = config.server.port
   const host = config.server.host
   app.listen(port, host, () => {
-    console.log('Server listening on :' + port)
+    logger.log('info', 'server listening on :' + port)
   })
 })()
