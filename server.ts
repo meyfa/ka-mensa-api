d32ef6788c76e80285e9a4193ab748d338cc5aa6
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-03-26 15:10:18 +0100

    feat: Use new 'omniwheel' dependency for startup/exit management
    Omniwheel is a new backend library I am developing to abstract away much
    of the boilerplate, such as the startup and shutdown code that was
    previously part of ka-mensa-api.

--- server.ts
+++ server.ts
@@ -1,4 +1,3 @@
-import { constants } from 'node:os'
 import express from 'express'
 import ms from 'ms'
 import cors from 'cors'
@@ -8,6 +7,7 @@ import { Cache } from './lib/cache.js'
 import { runFetchJob } from './lib/job.js'
 import { indexRoute } from './routes/index.js'
 import { logger } from './lib/logger.js'
+import { onTermination, promisifiedClose, promisifiedListen } from 'omniwheel'
 
 /**
  * Determine the CORS origin to allow, from either the environment variables or the config.
@@ -23,22 +23,11 @@ function getAllowOrigin (): string | undefined {
 }
 
 /**
- * Something that runs asynchronously in the background, such as a web server or timed job.
- */
-interface Service {
-  /**
-   * Shut down the service.
-   */
-  readonly stop: () => void | Promise<void>
-}
-
-/**
  * Run a fetch job in the background, with the interval set in the config.
  *
  * @param cache The plan cache to use.
- * @returns The job that was started.
  */
-async function startFetchJob (cache: Cache): Promise<Service> {
+async function startFetchJob (cache: Cache): Promise<void> {
   const period = ms(config.fetchJob.interval)
 
   // run the job once immediately, and then repeatedly
@@ -47,18 +36,15 @@ async function startFetchJob (cache: Cache): Promise<Service> {
     void runFetchJob(cache)
   }, period)
 
-  return {
-    stop: () => clearInterval(interval)
-  }
+  onTermination(() => clearInterval(interval))
 }
 
 /**
  * Start the web server.
  *
  * @param cache The plan cache to use.
- * @returns The server that was started.
  */
-async function startServer (cache: Cache): Promise<Service> {
+async function startServer (cache: Cache): Promise<void> {
   const app = express()
   const allowOrigin = getAllowOrigin()
   if (allowOrigin != null) {
@@ -68,43 +54,24 @@ async function startServer (cache: Cache): Promise<Service> {
 
   const port = config.server.port
   const host = config.server.host
+  const server = await promisifiedListen(app, port, host)
 
-  const server = app.listen(port, host, () => {
-    logger.log('info', `server listening on :${port}`)
-  })
+  logger.log('info', `Server listening on :${port}`)
 
-  return {
-    async stop () {
-      return await new Promise((resolve) => {
-        server.close(() => resolve())
-      })
-    }
-  }
+  onTermination(async () => await promisifiedClose(server))
 }
 
 /**
  * Application entrypoint.
  */
 async function main (): Promise<void> {
-  // set up our exit strategy as early as possible, so we can still abort when something hangs during startup
-  const activeServices: Service[] = []
-  for (const signal of ['SIGTERM', 'SIGINT'] as const) {
-    process.once(signal, () => {
-      console.log(`Received ${signal}, exiting`)
-      void Promise.all(activeServices.map(async (service) => await service.stop())).then(() => {
-        // process exit code is typically 128+signal on Linux, such as 143 for SIGTERM
-        process.exit(128 + constants.signals[signal])
-      })
-    })
-  }
-
   const fsAdapter = new DirectoryAdapter(config.cache.directory)
   await fsAdapter.init()
 
   const cache = new Cache(fsAdapter)
 
-  activeServices.push(await startFetchJob(cache))
-  activeServices.push(await startServer(cache))
+  await startFetchJob(cache)
+  await startServer(cache)
 }
 
 void main()
